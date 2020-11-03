c16f9d8bd7fecfae2d159018286baf15cdc07c88
yousuf <marui801234>
2020-11-03 16:16:05 +0800

    Initial commit

---
+++ spirng-studay-eureka-server/src/main/java/com/vastmoon/study/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.vastmoon.study.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p> ClassName: EurekaServerApplication
+ * <p> Description: 启动类
+ *
+ * @author yousuf 2020/11/3
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
